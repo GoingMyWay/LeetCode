32e0741aa8ea8598c797a0affa3ebc3cc8d65040
GoingMyWay <scutqiuwei@163.com>
2018-01-21 09:11:21 +0800

    599. Minimum Index Sum of Two Lists

---
+++ 599. Minimum Index Sum of Two Lists/solution.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        map<string, int> hashMap;
+        for ( int i = 0; i < list1.size(); i ++ ) {
+            hashMap.insert(pair<string, int>(list1[i], i));
+        }
+        int best = 1e9, tmp = 0;
+        vector<string> result;
+        for ( int j = 0; j < list2.size(); j ++) {
+            if ( 0 == hashMap.count(list2[j]) ) {
+                tmp = 1e9;
+            } else {
+                tmp = hashMap[list2[j]];
+            }
+            if ( j + tmp < best ) {
+                vector<string> res;
+                res.push_back(list2[j]);
+                result = res;
+                best = j + tmp;
+            } else if ( j + tmp == best ) {
+                result.push_back(list2[j]);
+            }
+        }
+        return result;
+    }
+};
