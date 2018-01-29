6a5cafc605f4246b2d092c6b72cafab15ed2d76d
GoingMyWay <scutqiuwei@163.com>
2018-01-29 22:34:43 +0800

    771. Jewels and Stones

---
+++ 771. Jewels and Stones/Solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        s = set(J)
+        return sum([v in s for v in S])
