21375719d34e6cb56ce9d9d2410f2ac67d6773f7
GoingMyWay <scutqiuwei@163.com>
2018-02-18 09:05:50 +0800

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string newS = "";
+        for (auto v: s) { 
+            if ((( v >= a && v <= z) || ( v >= A && v <= Z)) || (v >= 0 && v <= 9)) { 
+                newS += v;
+            }
+        }
+        transform(newS.begin(), newS.end(), newS.begin(), ::toupper);
+        int l = 0, r = newS.size()-1;
+        while ( l < r ) {
+            if (newS[l] == newS[r]) {
+                l ++;
+                r --;
+            } else return false;
+        }
+        return true;
+    }
+};
