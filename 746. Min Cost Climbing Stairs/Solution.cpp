e1f0329d1a2743a1609b22292d6029871bb41cfe
GoingMyWay <scutqiuwei@163.com>
2018-02-20 17:32:16 +0800

    746. Min Cost Climbing Stairs

--- 746. Min Cost Climbing Stairs/Solution.cpp
+++ 746. Min Cost Climbing Stairs/Solution.cpp
@@ -1,4 +1,5 @@
-class Solution {
+// Bottom-Top Solution
+class Solution1 {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int size = (int)cost.size();
@@ -11,3 +12,21 @@ public:
         return min(dp[size-2], dp[size-1]);
     }
 }; 
+
+// Top-Bottom Solution
+class Solution2 {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        return topDownDP(cost, cost.size());
+    }
+
+    int topDownDP(vector<int>& c, int idx) {
+        if ( 1 == dp.count(idx)) return dp[idx];
+        if ( idx <= 1 ) return dp[idx] = c[idx];
+        return dp[idx] = (idx == c.size() ? 0: c[idx]) + min(topDownDP(c, idx-1), topDownDP(c, idx-2));
+    }
+
+private:
+    unordered_map<int, int> dp;
+};
+
