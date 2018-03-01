1ae10adcf6086f04b6850cff3e9d15735bc59c8e
GoingMyWay <scutqiuwei@163.com>
2018-03-01 16:10:08 +0800

    451. Sort Characters By Frequency

--- 451. Sort Characters By Frequency/Solution.py
+++ 451. Sort Characters By Frequency/Solution.py
@@ -4,5 +4,5 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return .join(k*v for k, v in sorted(dict(collections.Counter(s)).items(), key=lambda v: v[1], reverse=True))
+        return ''.join(k*v for k, v in sorted(dict(collections.Counter(s)).items(), key=lambda v: v[1], reverse=True))
 
