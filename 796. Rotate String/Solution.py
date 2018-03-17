576212cee4cfe62d363f5c6634116de48d130c45
GoingMyWay <scutqiuwei@163.com>
2018-03-17 23:13:22 +0800

    796. Rotate String

---
+++ 796. Rotate String/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        for step in range(len(A)):
+            for idx in range(len(A)):
+                if A[(idx+step) % len(A)] != B[idx]:
+                    break
+            else:
+                return True
+        return False
+        
+
