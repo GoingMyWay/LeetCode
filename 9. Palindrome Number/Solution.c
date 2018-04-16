b60039b408511b8c946e02c7e13fa15f45234ef6
GoingMyWay <scutqiuwei@163.com>
2018-04-16 17:16:52 +0800

    9. Palindrome Number

---
+++ 9. Palindrome Number/Solution.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x) {
+    int a = x, r = 0;
+    if (x < 0)
+        return 0;
+    while (a > 0)
+    {
+        r = r*10 + a%10;
+        a /= 10;
+    }
+    
+    return r == x;
+}
