5f6b352a8574000bf349d2ef51006be7cae0819f
GoingMyWay <scutqiuwei@163.com>
2018-04-16 17:16:51 +0800

    349. Intersection of Two Arrays

---
+++ 349. Intersection of Two Arrays/Solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        result, m_dict = [], {}
+        for v1 in nums1:
+            m_dict[v1] = 1
+        
+        for k in m_dict.keys():
+            if k in nums2:
+                result.append(k)
+        return result
+        # return list(set(nums1) & set(nums2))
