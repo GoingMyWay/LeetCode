a9a344c9cc7f3cdddf9796b2af1f790483091606
GoingMyWay <scutqiuwei@163.com>
2018-04-16 17:16:37 +0800

    268. Missing Number

--- Missing Number/Solution.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        return reduce(operator.xor, nums+range(len(nums)+1))
-    
