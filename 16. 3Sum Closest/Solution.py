d0ddf1cc88bec0f367c1090d4d49fe5ecd4ef9eb
GoingMyWay <scutqiuwei@163.com>
2018-04-16 17:16:51 +0800

    16. 3Sum Closest

---
+++ 16. 3Sum Closest/Solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        result = 0
+        nums.sort()
+        min_gap = sys.maxint
+        
+        for i in range(len(nums)-2):
+            begin, end = i + 1, len(nums) - 1
+            
+            while begin < end:
+                sum_v = nums[i] + nums[begin] + nums[end]
+                
+                gap = abs(sum_v - target)
+                if gap < min_gap:
+                    result = sum_v
+                    min_gap = gap
+                
+                if sum_v > target:
+                    end -= 1
+                elif sum_v < target:
+                    begin += 1
+                else:
+                    return sum_v
+        return result
