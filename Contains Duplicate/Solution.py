1a724d16fd9690b788e47b4074b4ddc0a4be8aa4
GoingMyWay <scutqiuwei@163.com>
2018-04-16 17:16:38 +0800

    217. Contains Duplicate

--- Contains Duplicate/Solution.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # @param {integer[]} nums
-    # @return {boolean}
-    def containsDuplicate(self, nums):
-        if len(nums) == 0 or len(nums) == 1:
-            return False
-        if len(nums) == len(set(nums)):
-            return False
-        return True
