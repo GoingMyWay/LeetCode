40a52dc7fa6b5926a0016b7f065fa3f7644746ca
GoingMyWay <scutqiuwei@163.com>
2018-04-16 17:16:52 +0800

    532. K-diff Pairs in an Array

---
+++ 532. K-diff Pairs in an Array/Solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if len(nums) == 0 or k < 0:
+            return 0
+        
+        count, m_map = 0, {}
+        
+        for v in nums:
+            m_map[v] = m_map.get(v, 0) + 1
+        
+        for key in m_map.keys():
+            if k == 0:
+                if m_map.get(k+key, 0) >= 2:
+                    count += 1
+            else:
+                if k+key in m_map:
+                    count += 1
+        return count
