e2c07dbec17c40638bdd88d4a907d1287e722dd6
GoingMyWay <scutqiuwei@163.com>
2018-06-14 17:19:34 +0800

    804. Unique Morse Code Words

---
+++ 804. Unique Morse Code Words/Solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        return len(set([.join([morse[ord(c)-ord(a)] for c in w]) for w in words]))
+
