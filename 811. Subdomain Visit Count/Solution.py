e0803976c3c5041625677cef28de1fd0803b721c
GoingMyWay <scutqiuwei@163.com>
2018-07-11 13:18:55 +0800

    811. Subdomain Visit Count

--- 811. Subdomain Visit Count/Solution.py
+++ 811. Subdomain Visit Count/Solution.py
@@ -7,10 +7,10 @@ class Solution(object):
         temp_hash = {}
         for v in cpdomains:
             value, domain = v.split()
-            splited = domain.split(.)
-            keys = [..join(splited[i:]) for i in range(len(splited))]
+            splited = domain.split(".")
+            keys = [".".join(splited[i:]) for i in range(len(splited))]
             for k in keys:
                 temp_hash[k] = temp_hash.get(k, 0) + int(value)
                 
-        return [%s %s % (v, k) for k, v in temp_hash.items()]
+        return ["%s %s" % (v, k) for k, v in temp_hash.items()]
 
