767737ed140bba0888e1acecf4e3f9fdf99a0cfd
GoingMyWay <scutqiuwei@163.com>
2018-08-01 18:45:14 +0800

    876. Middle of the Linked List

--- 876. Middle of the Linked List/Solution.c
+++ 876. Middle of the Linked List/Solution.c
@@ -9,8 +9,7 @@ struct ListNode* middleNode(struct ListNode* head) {
     struct ListNode * fast = head;
     struct ListNode * slow = head;
     while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
+        slow = slow->next, fast = fast->next->next;
     }
     return slow; 
 }
