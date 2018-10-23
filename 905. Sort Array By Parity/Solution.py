0de20ceed9ad39c045b287ecbf6dea41c3cabe74
GoingMyWay <scutqiuwei@163.com>
2018-10-23 09:18:14 +0800

    905. Sort Array By Parity

---
+++ 905. Sort Array By Parity/Solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortArrayByParity(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for v in A:
+            if v % 2 == 0:
+                res.append(v)
+        for v in A:
+            if v % 2 != 0:
+                res.append(v)
+        return res;
+
