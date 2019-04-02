d7ad3569856859d4799b0ba0559db8e2a3f8d220
GoingMyWay <scutqiuwei@163.com>
2019-04-02 17:35:32 +0800

    113. Path Sum II

--- 113. Path Sum II/Solution.py
+++ 113. Path Sum II/Solution.py
@@ -9,7 +9,10 @@ class Solution:
     def pathSum(self, root: TreeNode, sum_v: int) -> List[List[int]]:
         if root is None:
             return []
-        return self.bfs(root, sum_v)
+        # return self.dfs(root, sum_v)
+        result = []
+        self.dfs_rec(root, sum_v, [], result)
+        return result
 
     def dfs(self, root: TreeNode, sum_v: int) -> List[List[int]]:
         # DFS + stack
@@ -17,12 +20,20 @@ class Solution:
         while len(stack) != 0:
             node, vals = stack.pop()
             if node.left is None and node.right is None and sum(vals) == sum_v:
-                result.append(vals[:])
+                result.append(vals)
             if node.right is not None:
                 stack.append((node.right, vals+[node.right.val]))
             if node.left is not None:
                 stack.append((node.left, vals+[node.left.val]))
         return result
+
+    def dfs_rec(self, node, sum_v, vals, result):
+        if not node.left and not node.right and sum_v == node.val:
+            result.append(vals+[node.val])
+        if node.left:
+            self.dfs_rec(node.left, sum_v-node.val, vals+[node.val], result)
+        if node.right:
+            self.dfs_rec(node.right, sum_v-node.val, vals+[node.val], result)
 
     def bfs(self, root: TreeNode, sum_v: int) -> List[List[int]]:
         # BFS + queue
