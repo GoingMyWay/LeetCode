7ba603a4289f2d6c85ecb05f35ef8b4dbfb38f4d
GoingMyWay <scutqiuwei@163.com>
2023-02-16 15:58:48 +0800

    greedy

---
+++ 55-jump-game/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int farthest = 0;
+        for (int i = 0; i < n - 1; i++) {
+            // 不断计算能跳到的最远距离
+            farthest = max(farthest, i + nums[i]);
+            // 可能碰到了 0，卡住跳不动了
+            if (farthest <= i) {
+                return false;
+            }
+        }
+        return farthest >= n - 1;
+    }
+};
