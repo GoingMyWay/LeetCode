907e28c05a051f1750ba85117e1901e9cd78cd5f
GoingMyWay <scutqiuwei@163.com>
2023-03-08 17:43:53 +0800

    add maximum-depth-of-n-ary-tree and minimize-max-distance-to-gas-station

---
+++ 788-minimize-max-distance-to-gas-station/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double minmaxGasDist(vector<int>& stations, int k) {
+        double left = 0, right = 1e8;
+        while (right - left > 1e-6) {
+            double mid = left + (right - left) / 2;
+            if (isPossible(mid, stations, k))
+                right = mid;
+            else
+                left = mid;
+        }
+        return left;
+    }
+
+private:
+    bool isPossible(double D, vector<int>& stations, int k) {
+        int used = 0;
+        for (int i = 0; i < stations.size() - 1; ++i)
+            used += (int) ((stations[i+1] - stations[i]) / D);
+        return used <= k;
+    }
+};
